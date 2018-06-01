938580e5b9d6e35f8f118896d6c37dfeaaf32cb6
ricardoavion <ricardo.aviador95@gmail.com>
2018-06-01 11:42:43 -0600

    Proabando commit en branch

--- app/src/main/java/com/example/avion/gamenews/MainActivity.java
+++ app/src/main/java/com/example/avion/gamenews/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.avion.gamenews;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    String hola;
+
 }
